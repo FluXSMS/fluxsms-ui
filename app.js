6124ff60e5e9e5c33eeee8299d7ec623ff819230
HridoyAdmin <hridoyytr04@gmail.com>
2025-07-14 11:51:24 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function getNumber() {
+  document.getElementById("output").innerText = "✅ Number assigned: +8801234567890";
+}
+function mute() {
+  document.getElementById("output").innerText = "🔕 Mute enabled.";
+}
+function unmute() {
+  document.getElementById("output").innerText = "🔔 Unmute enabled.";
+}
